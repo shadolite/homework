9b7b0d80feaaed029ceef612394fc00ab5859c44
shadolite <amy.kawaii@gmail.com>
2017-02-12 04:11:39 -0700

    Makefile update

--- TeamActivities/ta04/rational.cpp
+++
@@ -1,9 +0,0 @@
-/***************************************************************
- * File: rational.cpp
- * Author: (your name here)
- * Purpose: Contains the method implementations for the Rational class.
- ***************************************************************/
-
-#include "rational.h"
-
-// put your method bodies here
